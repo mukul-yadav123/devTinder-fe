6c94cfe6d573c3d4837460693a5fcf5c889dddcb
Mukul Yadav <mukuly2002@gmail.com>
2025-04-16 20:11:37 +0530

    Basic ui made

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./components/Body"
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter basename="/">
+    <Routes>
+      <Route path="/" element={<Body/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
