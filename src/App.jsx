dfc06415be95d8f09e02909857742441afe319cc
Mukul Yadav <mukuly2002@gmail.com>
2025-04-17 19:50:06 +0530

    Login component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Body from "./components/Body"
+import Login from "./components/Login"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
+import Feed from "./components/Feed"
 
 function App() {
 
   return (
     <>
+    <Provider store={appStore}>
     <BrowserRouter basename="/">
     <Routes>
-      <Route path="/" element={<Body/>}/>
+      <Route path="/" element={<Body/>}>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Feed/>}/>
+      </Route>
     </Routes>
     </BrowserRouter>
+    </Provider>
     </>
   )
 }
