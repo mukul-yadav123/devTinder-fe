7a5c9b2345fedc8a79d1d5c6ea6582e6310b4286
Mukul Yadav <mukuly2002@gmail.com>
2025-04-24 12:34:55 +0530

    Signup page added

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,24 +1,52 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
-const UserCard = ({user}) => {
+const UserCard = ({ user }) => {
+  console.log(user._id);
+  const dispatch = useDispatch();
+
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}request/send/${status}/${userId}`,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="card bg-base-300 w-96 shadow-sm">
-  <figure>
-    <img
-      src={user.photoUrl}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{user.firstName  + " " + user.lastName}</h2>
-    {user.age && user.gender && <p>{user.age + " " + user.gender}</p>}
-    <p>{user.about}</p>
-    <div className="card-actions justify-center my-4">
-      <button className="btn btn-primary">Ignore</button>
-      <button className="btn btn-secondary">Interested</button>
+      <figure>
+        <img src={user.photoUrl} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{user.firstName + " " + user.lastName}</h2>
+        {user.age && user.gender && <p>{user.age + " " + user.gender}</p>}
+        <p>{user.about}</p>
+        <div className="card-actions justify-center my-4">
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", user._id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", user._id)}
+          >
+            Interested
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
